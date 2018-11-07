59199676da058d1c3b1295bea4dfc4d97371a45c
katujavasparow <katujavasparow@gmail.com>
2018-11-07 00:13:11 -0500

    message

--- ex06-6.js
+++ ex06-6.js
@@ -1,14 +1,14 @@
-var who66 ='world';
-//var come = 'が来た';
-function sayhello66(){
-//var who = 'world';
-  alert('Hello'+ who66 +'が来た!');
+var who66 = 'world';
+// var come = 'が来た';
+function sayhello66() {
+// var who = 'world';
+  alert('Hello, ' + who66 + '!');
 }
-function taro66(){
+function taro() {
   who66 = '太郎';
 }
 
-function hanako66(){
+function hanako() {
   who66 = '花子';
 }
 function gakita() {
